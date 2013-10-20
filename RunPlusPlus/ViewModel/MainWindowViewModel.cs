f61e926b310e4705b89e9573f2a339ba691350d4
HenryZhu <SND\HenryZhu_cp>
2013-10-20 07:44:06 +0000

    Created the UI and ViewModels

---
+++ RunPlusPlus/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,99 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using RunPlusPlus.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunPlusPlus.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+
+        public MainWindowViewModel()
+        {
+            this.InitializeCommands();
+        }
+
+        private void InitializeCommands()
+        {
+            this.AddCommand = new RelayCommand(this.AddItem);
+            this.RemoveCommand = new RelayCommand(this.RemoveItem,
+                () =>
+                {
+                    return this.SelectedItem != null;
+                });
+
+            this.SaveCommand = new RelayCommand(this.SaveItem,
+                () =>
+                {
+                    return this.SelectedItem == null ? 
+                        false : 
+                        !string.IsNullOrEmpty(this.SelectedItem.Shortcut);
+                });
+        }
+
+        private ObservableCollection<ShortcutViewModel> _shorts = new ObservableCollection<ShortcutViewModel>();
+
+        public ObservableCollection<ShortcutViewModel> Shortcuts
+        {
+            get { return _shorts; }
+            set
+            {
+                _shorts = value;
+                this.RaisePropertyChanged("Shortcuts");
+            }
+        }
+
+        private ShortcutViewModel _selectedItem;
+
+        public ShortcutViewModel SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                _selectedItem = value;
+                this.RaisePropertyChanged("SelectedItem");
+            }
+        }
+
+
+        public RelayCommand AddCommand { get; set; }
+
+        public RelayCommand RemoveCommand { get; set; }
+
+        public RelayCommand ShowAboutCommand { get; set; }
+
+        public RelayCommand SaveCommand { get; set; }
+
+        private void AddItem()
+        {
+            var item = new ShortcutViewModel();
+            this.Shortcuts.Add(item);
+            this.SelectedItem = item;
+        }
+
+        private void RemoveItem()
+        {
+            var index = this.Shortcuts.IndexOf(this.SelectedItem);
+            this.SelectedItem.Delete();
+            this.Shortcuts.Remove(this.SelectedItem);
+            if ((index + 1) > this.Shortcuts.Count)
+            {
+                index--;
+            }
+            if (this.Shortcuts.Count != 0)
+            {
+                this.SelectedItem = this.Shortcuts[index];
+            }
+        }
+
+        private void SaveItem()
+        {
+            this.SelectedItem.Save();
+        }
+    }
+}
---
+++ RunPlusPlus/ViewModel/ShortcutViewModel.cs
@@ -0,0 +1,85 @@
+﻿using GalaSoft.MvvmLight;
+using RunPlusPlus.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunPlusPlus.ViewModel
+{
+    class ShortcutViewModel : ViewModelBase
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                this.RaisePropertyChanged("Name");
+            }
+        }
+
+        private string _shortcut;
+
+        public string Shortcut
+        {
+            get { return _shortcut; }
+            set
+            {
+                _shortcut = value;
+                this.RaisePropertyChanged("Shortcut");
+            }
+        }
+
+        private string _command;
+
+        public string Command
+        {
+            get { return _command; }
+            set
+            {
+                _command = value;
+                this.RaisePropertyChanged("Command");
+            }
+        }
+
+        private bool _isRunAsAdministrstor;
+
+        public bool IsRunAsAdministrator
+        {
+            get { return _isRunAsAdministrstor; }
+            set
+            {
+                _isRunAsAdministrstor = value;
+                this.RaisePropertyChanged("IsRunAsAdministrator");
+            }
+        }
+
+        private WindowType _windowType;
+
+        public WindowType WindowType
+        {
+            get { return _windowType; }
+            set
+            {
+                _windowType = value;
+                this.RaisePropertyChanged("WindowType");
+            }
+        }
+
+        public void Save()
+        {
+
+        }
+
+
+
+        public void Delete()
+        {
+
+        }
+    }
+}
